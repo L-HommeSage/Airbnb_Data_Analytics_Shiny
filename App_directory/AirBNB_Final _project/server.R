28ca2e14f139840618e44dd917c6e15a4c26bab1
ThomLlobEce <36538693+ThomLlobEce@users.noreply.github.com>
2020-11-21 19:09:55 +0100

    Loading on top of server launch

--- App_directory/AirBNB_Final _project/server.R
+++ App_directory/AirBNB_Final _project/server.R
@@ -1,12 +1,16 @@
 library(shiny)
+source("getGzCsv.R")
 
 function(input, output) {
-  output$plot <- renderPlot({
-    plot(rnorm(input$num), type=input$plotType)
-  })
-  output$plot2 <- renderPlot({
-    plot(rnorm(input$num2), type=input$plotType2)
+  #berlin_data <- get_data("http://data.insideairbnb.com/germany/be/berlin/2020-10-13/data/listings.csv.gz", "http://data.insideairbnb.com/germany/be/berlin/2020-10-13/data/calendar.csv.gz")
+  #bologna_data <- get_data("http://data.insideairbnb.com/italy/emilia-romagna/bologna/2020-10-24/data/listings.csv.gz", "http://data.insideairbnb.com/italy/emilia-romagna/bologna/2020-10-24/data/calendar.csv.gz")
+  #london_data <- get_data("http://data.insideairbnb.com/united-kingdom/england/london/2020-10-13/data/listings.csv.gz", "http://data.insideairbnb.com/italy/emilia-romagna/bologna/2020-10-24/data/calendar.csv.gz")
+  
+  output$city_one <- renderText({ 
+    paste("You have selected this", input$city_one)
   })
   
-  
+  output$city_two <- renderText({ 
+    paste("You have selected this", input$city_two)
+  })
 }
